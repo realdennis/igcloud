8fae9adce251a6384927417c011a6383a459f656
Dennis <dennis@Dennisde-MacBook-Pro.local>
2018-02-12 14:50:12 +0800

    remove jieba debug message

--- igcloud.py
+++ igcloud.py
@@ -38,7 +38,8 @@ if __name__ == '__main__':
 		print(''+startMessage)
 
 		allword=query.start(UserID)
-
+		jieba.setLogLevel(20)
+		#Don't show the jieba debug message
 		seg_list=jieba.cut(allword,cut_all=False)
 		wl_space_split=" ".join(seg_list)
 		my_wordcloud=WordCloud(font_path="noto.otf",background_color="white",width=640, height=480, margin=2).generate(wl_space_split)
@@ -46,6 +47,5 @@ if __name__ == '__main__':
 		plt.imshow(my_wordcloud)
 		plt.axis("off")
 		plt.savefig(str(UserID)+'.png',dpi=1000)
-		#print('################')
 		print(doneMessage)
 		print('Done! the igcloud save as '+str(UserID)+'.png')
